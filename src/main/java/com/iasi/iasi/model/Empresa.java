1f588ef12831be2d6e37a6c615a0d92698083d44
Marcelo Schoen <mschoen@alvarezandmarsal.com>
2024-09-13 16:46:58 -0300

    First stable build

---
+++ src/main/java/com/iasi/iasi/model/Empresa.java
@@ -0,0 +1,45 @@
+package com.iasi.iasi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TB_IASI_EMPRESA")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_EMPRESA")
+    private long id;
+
+    @Column(name = "NOME_EMPRESA")
+    private String nome;
+
+    @Column(name = "SETOR_INDUSTRIAL_EMPRESA")
+    private String setorIndustrial;
+
+    @Column(name = "LOCALIZACAO_EMPRESA")
+    private String localizacao;
+
+    @Column(name = "TIPO_EMPRESA")
+    private String tipo;
+
+    @Column(name = "CONFORMIDADE_REGULAMENTAR")
+    private String conformidadeRegular;
+
+    public Empresa(String nome, String setorIndustrial, String localizacao, String tipo, String conformidadeRegular) {
+        this.nome = nome;
+        this.setorIndustrial = setorIndustrial;
+        this.localizacao = localizacao;
+        this.tipo = tipo;
+        this.conformidadeRegular = conformidadeRegular;
+    }
+
+}
